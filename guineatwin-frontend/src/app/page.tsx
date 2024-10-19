c812e2cc8b8f0b0cbb53917373254d459e525042
garvcodes <garvgoswami@berkeley.edu>
2024-10-18 22:56:04 -0700

    landing page

--- guineatwin-frontend/src/app/page.tsx
+++ guineatwin-frontend/src/app/page.tsx
@@ -1,101 +1,88 @@
+'use client'
 import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="bg-gradient-to-br from-green-700 to-black">
+      <div className="text-gray-300 container mx-auto p-8 overflow-hidden md:rounded-lg md:p-10 lg:p-12">
+        <div className="flex justify-between">
+          <h1 className="font-serif text-3xl font-medium">PepsiCo Sustainability Challenge</h1>
+          
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className="h-32 md:h-40"></div>
+
+        <p className="font-sans text-4xl font-bold text-gray-200 max-w-5xl lg:text-7xl lg:pr-24 md:text-6xl">
+        Show us what you can create using America's favorite drink!
+        </p>
+        <div className="h-10"></div>
+        <p className="max-w-2xl font-serif text-xl text-gray-400 md:text-2xl">
+          PepsiCo has been fueling American creativity for years, we're asking you to give us a little of that 
+          creativity in the form of Sustainable fun!
+        </p>
+
+        <div className="h-32 md:h-40"></div>
+
+        <div className="grid gap-8 md:grid-cols-2">
+          <div className="flex flex-col justify-center">
+            <p className="self-start inline font-sans text-xl font-medium text-transparent bg-clip-text bg-gradient-to-br from-green-400 to-green-600">
+              How it Works
+            </p>
+            <h2 className="text-4xl font-bold">It starts with a picture...</h2>
+            <div className="h-6"></div>
+            <p className="font-serif text-xl text-gray-400 md:pr-10">
+              After uploading a picture of your PepsiCo product, it will undergo analysis using the Google Gemini API. 
+              Gemini will then issue you a challenge... 
+              A challenge to make something new out of something old! Using the prompt, gemini will create it's own rendition as well which 
+              you can use as inspiration in your creation!
+            </p>
+            <div className="h-8"></div>
+            <div className="grid grid-cols-2 gap-4 pt-8 border-t border-green-700">
+
+              {/* Section for gemini picture examples*/}
+              {/* Picture 1*/}
+              <div>
+                <p className="font-semibold text-gray-400">Gemini's Image</p>
+                <div className="h-4">
+                <Image
+      src="/pepsiplanter.png"
+      width={500}
+      height={500}
+      alt="Picture of the author"
+    />
+                </div>
+                <p className="font-serif text-gray-400">
+          
+                </p>
+              </div>
+
+            {/* Picture 2*/}
+              <div>
+                <p className="font-semibold text-gray-400">Your Creation!</p>
+                <div className="h-4"></div>
+                <p className="font-serif text-gray-400">
+              
+                </p>
+              </div>
+
+
+
+
+            </div>
+          </div>
+          <div>
+            <div className="-mr-24 rounded-lg md:rounded-l-full bg-gradient-to-br from-red-400 to-blue-700 h-96"></div>
+          </div>
+        </div>
+
+
+
+        <div className="flex justify-center pt-12 pb-8 text-gray-400">
+          © 2024 All rights reserved
+        </div>
+        </div>
     </div>
+
   );
-}
+};
